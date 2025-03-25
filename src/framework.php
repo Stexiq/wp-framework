90312cd3b715fb3753cf6dc97982f941026fa09c
SyntaxDTL <admin@syntax.digital>
2025-03-25 15:31:50 +0100

    Update framework.php

--- src/framework.php
+++ src/framework.php
@@ -1,5 +1,6 @@
 <?php
 define( 'SX_ROOT', plugin_dir_path(__FILE__) );
+define( 'SX_URL', plugin_dir_url(__FILE__) );
 
 if( ! function_exists( 'get_plugins' ) ) {
 	require_once ABSPATH . 'wp-admin/includes/plugin.php';
@@ -25,3 +26,20 @@ require_once SX_ROOT . 'fields/text.php';
 require_once SX_ROOT . 'fields/custom.php';
 require_once SX_ROOT . 'fields/repeat.php';
 require_once SX_ROOT . 'fields/tab.php';
+
+add_action( 'admin_enqueue_scripts', 'sx_enqueue_scripts' );
+
+function sx_enqueue_scripts()
+{
+	wp_enqueue_style( 'sx-admin-css', SX_URL . '/assets/css/admin.css', array(), '1.0', 'all' );
+
+	$required = array(  );
+	if( wp_script_is( 'color-picker-js', 'enqueued' ) ) {
+		$required[] = 'color-picker-js';
+	}
+	wp_enqueue_script( 'sx-admin-js', SX_URL . '/assets/js/admin.min.js', $required, '1.0', true );
+	wp_localize_script( 'sx-admin-js', 'tk', array(
+		'ajax_url' => admin_url( 'admin-ajax.php' ),
+		'nonce' => wp_create_nonce( 'sx' ),
+	) );
+}
