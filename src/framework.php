d1dcbf5551a70e9c3fd80a5582e6350af59aae1a
SyntaxDTL <admin@syntax.digital>
2025-03-25 13:33:03 +0100

    Update framework.php

--- src/framework.php
+++ src/framework.php
@@ -8,7 +8,19 @@ if( ! function_exists( 'get_plugins' ) ) {
 	require_once ABSPATH . 'wp-admin/includes/plugin.php';
 }
 
-require_once SX_ROOT . '/functions.php';
-require_once SX_ROOT . '/classes/class-lb-plugin.php';
-require_once SX_ROOT . '/classes/class-lb-register.php';
-require_once SX_ROOT . '/classes/class-lb-admin.php';
+require_once SX_ROOT . 'includes/framework/functions.php';
+
+require_once SX_ROOT . 'includes/framework/classes/class-plugin.php';
+require_once SX_ROOT . 'includes/framework/classes/class-wv-field.php';
+
+require_once SX_ROOT . 'includes/framework/fields/select.php';
+require_once SX_ROOT . 'includes/framework/fields/section.php';
+require_once SX_ROOT . 'includes/framework/fields/checkbox.php';
+require_once SX_ROOT . 'includes/framework/fields/textarea.php';
+require_once SX_ROOT . 'includes/framework/fields/switch.php';
+require_once SX_ROOT . 'includes/framework/fields/color.php';
+require_once SX_ROOT . 'includes/framework/fields/password.php';
+require_once SX_ROOT . 'includes/framework/fields/text.php';
+require_once SX_ROOT . 'includes/framework/fields/custom.php';
+require_once SX_ROOT . 'includes/framework/fields/repeat.php';
+require_once SX_ROOT . 'includes/framework/fields/tab.php';
