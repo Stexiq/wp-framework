32dede2412bd37659533daaa0dba81499296e84f
SyntaxDTL <admin@syntax.digital>
2025-03-28 20:06:59 +0100

    Base

--- src/framework.php
+++ src/framework.php
@@ -3,6 +3,7 @@ define( 'SX_ROOT', plugin_dir_path(__FILE__) );
 define( 'SX_URL', plugin_dir_url(__FILE__) );
 define( 'SX_PREFIX', 'sx' );
 define( 'SX_OPTION_SLUG', SX_PREFIX . '_options' );
+define( 'SX_TEXT_DOMAIN', 'sx' );
 
 if( ! function_exists( 'get_plugins' ) ) {
 	require_once ABSPATH . 'wp-admin/includes/plugin.php';
